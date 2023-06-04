ff9e970de72252411ab778e04eb7f3f8e35ff822
Hasnathasan <hasnatoooooooo@gmail.com>
2023-06-04 16:22:27 +0600

    users get api added

--- index.js
+++ index.js
@@ -41,7 +41,10 @@ async function run() {
         res.send(result)
     })
 
-
+    app.get('/users', async(req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
     app.post("/users", async(req, res) => {
       const user = req.body;
       const query = {email: user.email}
