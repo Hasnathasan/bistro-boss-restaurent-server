3d7050922397d3c57960e93a3e3e68f3bc4ecba1
Hasnathasan <hasnatoooooooo@gmail.com>
2023-06-04 23:37:12 +0600

    jwt token get, post and verifyJWT added

--- index.js
+++ index.js
@@ -9,7 +9,20 @@ const port = process.env.PORT || 5000;
 // Middlewares
 app.use(cors())
 app.use(express.json())
-
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: "unthorized user"})
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.JWT_ACCESSTOKEN, (err, decoded) => {
+    if(err){
+      return res.status(401).send({error: true, message: "unthorized user"})
+    }
+    req.decoded = decoded;
+    next()
+  })
+}
 
 
 
@@ -45,10 +58,11 @@ async function run() {
     app.post('/jwt', (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.JWT_ACCESSTOKEN, { expiresIn: '1h' });
-      res.send(token)
+      res.send({token})
     })
 
     app.get('/users', async(req, res) => {
+      
       const result = await usersCollection.find().toArray();
       res.send(result)
     })
@@ -88,10 +102,10 @@ async function run() {
         const result = await reviewCollection.find().toArray();
         res.send(result)
     })
-    app.get('/carts', async(req, res) => {
+    app.get('/carts', verifyJWT, async(req, res) => {
       const email = req.query.email;
-      if(!email){
-        res.send([])
+      if(email !== req.decoded.email){
+        return res.status(403).send({error: true, message: "unthorized user"})
       }
       const query = { addedBy: email }
         const result = await cartCollection.find(query).toArray();
